bb72321d90590160222835f2722cfb51c85662fc
Visual Ehrmanntraut <30368284+VisualEhrmanntraut@users.noreply.github.com>
2023-10-11 22:29:59 +0300

    Hopefully fix Linux compilation
    Signed-off-by: Visual Ehrmanntraut <30368284+VisualEhrmanntraut@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-//  Copyright © 2022-2023 ChefKiss Inc. Licensed under the Thou Shalt Not Profit License version 1.0. See LICENSE for
-//  details.
+//!  Copyright © 2022-2023 ChefKiss Inc. Licensed under the Thou Shalt Not Profit License version 1.5.
+//!  See LICENSE for details.
 
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 #![deny(warnings, clippy::cargo, clippy::nursery, unused_extern_crates)]
--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,6 @@
+//!  Copyright © 2022-2023 ChefKiss Inc. Licensed under the Thou Shalt Not Profit License version 1.5.
+//!  See LICENSE for details.
+
 use plist::Value;
 
 #[must_use]
