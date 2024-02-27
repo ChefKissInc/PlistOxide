00ce71135f1a87e797df5649d29a27acefa92790
Visual Ehrmanntraut <30368284+VisualEhrmanntraut@users.noreply.github.com>
2024-02-27 10:14:55 +0200

    Update copyright headers
    skip-checks: true
    Signed-off-by: Visual Ehrmanntraut <30368284+VisualEhrmanntraut@users.noreply.github.com>

--- build.rs
+++ build.rs
@@ -1,3 +1,6 @@
+//!  Copyright © 2022-2024 ChefKiss Inc. Licensed under the Thou Shalt Not Profit License version 1.5.
+//!  See LICENSE for details.
+
 fn main() {
     println!("cargo:rustc-env=MACOSX_DEPLOYMENT_TARGET=10.13");
 }
